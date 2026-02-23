897ac33f84a424c75d8b8c8344ae1124908bb370
enganisamohamed <enganisamohamed@gmail.com>
2026-02-23 08:57:16 -0800

    all excercise added

---
+++ mainjs4.js
@@ -0,0 +1,18 @@
+// Function Basics
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(2, 3));
+console.log(add(10, 5));
+console.log(add(7, 8));
+
+
+// Function Expressions
+const add2 = function(a, b) {
+  return a + b;
+};
+
+console.log(add2(4, 6));
+console.log(add2(1, 9));
+console.log(add2(20, 5));
